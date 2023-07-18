272a04cb8e68cab4389f1db6c108400a8995148e
Cassandra <cassandrapw@icloud.com>
2023-07-18 21:04:08 +0200

    Update 2R_malabi_dyslexia_detection.R

--- scripts/2R_malabi_dyslexia_detection.R
+++ scripts/2R_malabi_dyslexia_detection.R
@@ -1,20 +1,27 @@
+# -------------------------------
+# Title: Dyslexia type summary
+# Author: Cassandra Potier Watkins
+
 # The goal of this script is to assess the dyslexias of a student.
+# ------------------------------- 
 
+# Load required packages
+# -------------------------------
 library(tidyr); library(readr); library(readxl)
-#source("global_functions.R")
-dysTypes <- read_excel("3_stim/malabi_errorcodes_french2.xlsx") 
-#dysTypes<- dysTypes[dysTypes$dyslexia!="visual"|dysTypes$errorcode!="bom", ]
-#dysTypes<- dysTypes[dysTypes$dyslexia!="visual"|dysTypes$errorcode!="bod", ]
-#dysTypes<- dysTypes[dysTypes$dyslexia!="visual"|dysTypes$errorcode!="bsub", ]
-norms <- read_csv("3_stim/malabi_norms_g67.csv")
+source("global_functions.R")
 
-# Put the single participant here.
-source <- "marie"
+# Load data
+# -------------------------------
+dysTypes <- read_excel("data/malabi_errorcodes_french2.xlsx") 
+norms <- read_csv("data/malabi_norms_g67.csv")
+# The subject.
+path <- 
 subj <- "40KdGx"
 grade <- 6
+a <- read_csv(paste0(path,"/malabi_",subj,".csv"))
 
-########################
-a <- read_csv(paste0("6_data/",source,"/malabi_",subj,".csv"))
+# Analyses
+# -------------------------------
 # Count the number of errors made by the subject for each error type.
 dys_code_list <- unique(c(as.list(dysTypes$errorcode)))
 dys_codes_wide <-sapply(dys_code_list, function(i){ x<-ifelse(grepl(pattern=i, a$errorcode), 1, 0)})
@@ -34,8 +41,9 @@ d_long$grade <- grade
 dyslexia_score <- aggregate(errors~ id + grade + subtest + dyslexia, d_long, sum)
 dyslexia_score <- merge(dyslexia_score, norms[,c("grade","subtest","dyslexia","CRAW")] )
 
-# Errors made for each dyslexia.
-dyslexia_score$dyslexia<- factor(dyslexia_score$dyslexia, levels=c("attentional","letter_position","neglect","phonological",
+# Errors made for each dyslexia ?
+dyslexia_score$dyslexia<- factor(dyslexia_score$dyslexia, 
+                                 levels=c("attentional","letter_position","neglect","phonological",
                                                                    "visual","vowel", "deep","surface"), ordered = TRUE)
 dyslexia_score %>%
   dplyr::select(subtest, dyslexia, errors) %>%
@@ -45,31 +53,6 @@ dyslexia_score %>%
 # Dyslexias found?
 dyslexia_score[dyslexia_score$errors>=dyslexia_score$CRAW, ]
 
-
-
-
-
-################## All ErrorCodes #############################@
-# The goal of this script is to assess the dyslexias of a student.
-
-library(tidyr); library(readr); library(readxl)
-
-norms <- read_csv("3_stim/malabi_errorcodenorms_g67.csv")
-
-
-########################
-a <- read_csv(paste0("6_data/",source,"/malabi_",subj,".csv"))
-# Count the number of errors made by the subject for each error type.
-dys_code_list <- unique(c(as.list(norms$errorcode)))
-dys_codes_wide <-sapply(dys_code_list, function(i){ x<-ifelse(grepl(pattern=i, a$errorcode), 1, 0)})
-colnames(dys_codes_wide) <- dys_code_list
-dys_codes_wide <- cbind(a[,c('subtest', 'id')], dys_codes_wide)
-d_long <- dys_codes_wide %>% pivot_longer(!c("id", "subtest"), names_to="errorcode",values_to="count") %>%
-  group_by(id, subtest, errorcode) %>% 
-  summarise(errors = sum(count), .groups = 'drop')
-
-# Add the subjects grade
-d_long$grade <- grade
 
 # Aggregate these error codes to the different dyslexias.
 dyslexia_score <- aggregate(errors~ id + grade + subtest + errorcode, d_long, sum)
